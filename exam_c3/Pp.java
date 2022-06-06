7240f652f8c258f3d2a09cffdc0c9b8566404196
Pranshu Pal <47880133+Pranshu4@users.noreply.github.com>
2022-06-06 15:58:44 +0530

    Add files via upload

--- exam_c3/Pp.java
+++ exam_c3/Pp.java
@@ -1,5 +1,24 @@
 package exam_c3;
-
+import java.util.Scanner;
 public class Pp {
-
+	public static void main(String[] args) {
+		
+		Scanner s1= new Scanner(System.in);
+		System.out.println("Enter The Length of Array");
+		int[] a=new int[s1.nextInt()];
+		for (int i=0;i<a.length;i++) {
+			a[i]=s1.nextInt();
+		}
+		System.out.println("Enter the index you want");
+		try {
+			int num =s1.nextInt();
+			System.out.println("The array element at index"+num+"="+a[num]);
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("java.lang.ArrayIndexOutOfBoundsException");
+		}
+		catch(NumberFormatException e) {
+			System.out.println("java.lang.NumberFormatException");
+		}
+	}
 }
